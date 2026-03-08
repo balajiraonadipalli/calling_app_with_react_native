8e7aa1e50297a095ac5d15da3f10f439c96db349
balajiraonadipalli <143882626+balajiraonadipalli@users.noreply.github.com>
2026-03-08 12:39:19 +0530

    Remove Firebase configuration from firebase.js
    Removed sensitive Firebase configuration details.

--- frontend/config/firebase.js
+++ frontend/config/firebase.js
@@ -6,13 +6,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration using Expo Public Env Vars
 const firebaseConfig = {
-    apiKey: "AIzaSyDRankpiKxegDHREkcZZuCrKl3u558s89U",
-    authDomain: "callingfeature-b2a3c.firebaseapp.com",
-    projectId: "callingfeature-b2a3c",
-    storageBucket: "callingfeature-b2a3c.firebasestorage.app",
-    messagingSenderId: "335346617782",
-    appId: "1:335346617782:web:fd7ea0326ffe84b4924517",
-    measurementId: "G-K1SE7V84QW"
+   
 };
 
 // Initialize Firebase
